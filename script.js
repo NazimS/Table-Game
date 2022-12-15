11d63fe6a9e16d9b96121a3c5b9805aaf28787f6
NazimS <nazim.shixkerimov@gmail.com>
2022-12-15 22:35:07 +0400

    fully finished

--- script.js
+++ script.js
@@ -1,45 +1,58 @@
+    
+    const cedvel = document.getElementById("cedvel");
+    const counter = document.getElementById("counter");
+    const result = document.getElementById("result");
+    let count = 20;
+    let kod = '';
+    let arr = [];
+    let n = 1;
 
-const cedvel = document.getElementById("cedvel");
-const counter = document.getElementById("counter");
-const result = document.getElementById("result");
-let count = 10;
-let kod = '';
-let arr = [];
-let n = 1;
-
-for (let i = 1; i <= 16; i++) {
-    arr.push(i);
-}
-counter.innerHTML = count;
-
-setInterval(() => {
-    if (count > 0) {
-        count-- ;
+    for (let i = 1; i <= 16; i++) {
+        arr.push(i);
+    }
     counter.innerHTML = count;
-    } else {result.innerHTML = "vaxt bitdi"}
-}, 1000);
-   
 
-for (let i = 1; i <= 4; i++) {
-    kod += "<tr>"
-        for (let k = 1; k <= 4; k++) {
-            let x = rand(0, arr.length - 1);
-            kod += `<td onclick = "vur(this)">${arr[x]}</td>`;
-            arr.splice(x, 1);
-        }
-    kod += "</tr>"
-}
+    let timer = setInterval(saat, 1000) ;
+
+   // design table with random units
+    for (let i = 1; i <= 4; i++) {
+        kod += "<tr>"
+            for (let k = 1; k <= 4; k++) {
+                let x = rand(0, arr.length - 1);
+                kod += `<td onclick = "press(this)">${arr[x]}</td>`;
+                arr.splice(x, 1);
+            }
+        kod += "</tr>"
+    }
     cedvel.innerHTML = kod;
 
-function vur(elm) {
-    // alert("salam");
-    // console.log(elm);
-    if (elm.innerHTML == n) {
-        elm.style.background = "skyblue" 
-        n++
+    // timer activities
+    function saat() {
+        if (count > 0) {
+            count-- ;
+            counter.innerHTML = count;
+        } else stop("Game Over !! Your time is up !!");
     }
-}    
+    
+    // stop timer and show relevant message
+    function stop(msg) {
+        clearInterval(timer);
+        result.innerHTML = msg;
+        n = 18; // stop clicking function after last number
+    }
 
-function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+    // press table cells, change bg color and increase array numbers
+    function press(elm) {
+
+        if (elm.innerHTML == n) {
+            elm.style.background = "#83D475" ;
+            n++ ;
+        }
+
+        // finish pressing all table cells
+        if (n == 17) stop("Congratulations !! You've succesfully finished");
+    }    
+
+    function rand(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
