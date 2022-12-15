2c904691e1dab7eabc8dfdcb7273253109e243c2
NazimS <nazim.shixkerimov@gmail.com>
2022-12-15 20:04:32 +0400

    timer added

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 
 const cedvel = document.getElementById("cedvel");
+const counter = document.getElementById("counter");
+const result = document.getElementById("result");
+let count = 10;
 let kod = '';
 let arr = [];
+let n = 1;
 
 for (let i = 1; i <= 16; i++) {
     arr.push(i);
 }
+counter.innerHTML = count;
+
+setInterval(() => {
+    if (count > 0) {
+        count-- ;
+    counter.innerHTML = count;
+    } else {result.innerHTML = "vaxt bitdi"}
+}, 1000);
+   
 
 for (let i = 1; i <= 4; i++) {
     kod += "<tr>"
         for (let k = 1; k <= 4; k++) {
             let x = rand(0, arr.length - 1);
-            kod += `<td onclick = "vur()">${arr[x]}</td>`;
+            kod += `<td onclick = "vur(this)">${arr[x]}</td>`;
             arr.splice(x, 1);
         }
     kod += "</tr>"
@@ -19,7 +32,12 @@ for (let i = 1; i <= 4; i++) {
     cedvel.innerHTML = kod;
 
 function vur(elm) {
-    alert("salam");
+    // alert("salam");
+    // console.log(elm);
+    if (elm.innerHTML == n) {
+        elm.style.background = "skyblue" 
+        n++
+    }
 }    
 
 function rand(min, max) {
